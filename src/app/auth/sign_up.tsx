37a0543f3ce26c4356287b52d2b67c24c0fd3e45
hiroshi <210814887+Smart-Site-Works@users.noreply.github.com>
2025-05-10 13:36:40 +0900

    Create Navigation from log_in screen and sign_up screen to memo list screen

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -2,10 +2,15 @@ import {
   View, Text, TextInput,
   TouchableOpacity, StyleSheet
 } from 'react-native'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 
 import Header from '../../components/Header'
 import Button from '../../components/Button'
+
+const handlePress = (): void => {
+  // 会員登録
+  router.push('/memo/list')
+}
 
 const SignUp = (): React.JSX.Element => {
   return (
@@ -15,7 +20,7 @@ const SignUp = (): React.JSX.Element => {
         <Text style={styles.title}>Sign Up</Text>
         <TextInput style={styles.input} value='Email address' />
         <TextInput style={styles.input} value='Password' />
-        <Button label='submit' />
+        <Button label='submit' onPress={handlePress} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already registered?</Text>
           <Link href='/auth/log_in' asChild>
