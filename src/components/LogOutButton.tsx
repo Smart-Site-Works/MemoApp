5fcd45ea51ca3115041e9c53e391006085526284
hiroshi <210814887+Smart-Site-Works@users.noreply.github.com>
2025-05-12 14:33:06 +0900

    Implement logout

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -1,8 +1,25 @@
-import { TouchableOpacity, Text, StyleSheet } from "react-native"
+import {
+  TouchableOpacity, Text,
+  Alert, StyleSheet
+} from "react-native"
+import { router } from "expo-router"
+import { signOut } from "firebase/auth"
+
+import { auth } from "../config"
+
+const handlePress = (): void => {
+  signOut(auth)
+    .then(() => {
+      router.replace('auth/log_in')
+    })
+    .catch(() => {
+      Alert.alert('ログアウトに失敗しました')
+    })
+}
 
 const LogOutButton = (): React.JSX.Element => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={handlePress}>
       <Text style={styles.text}>ログアウト</Text>
     </TouchableOpacity>
   )
